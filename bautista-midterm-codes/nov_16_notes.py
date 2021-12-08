a1e4ba611277a524c4398b4707e092f827b76f23
A. Bautista <bautista_3191017@letran-calamba.edu.ph>
2021-12-08 09:51:53 +0800

    nov_16_codes

---
+++ bautista-midterm-codes/nov_16_notes.py
@@ -0,0 +1,78 @@
+#SPLIT FUNCTION
+#str = "a-b-c-d-e-f-g-h-i-j-k, lmnop"
+
+#parse_str = str.split('-') # dash is the delimiter
+#print (parse_str)
+
+#parse_str = str.split(',')
+#print (parse_str[0].split('-'))
+#print (parse_str[1])
+
+
+#LIST
+#init_list = [1, 2, 3, 4]
+#init_list.append(5)
+#init_list.append([6, 7, 8, 9])
+#print(init_list)
+#print(init_list[2])
+
+
+#TRY
+#try:
+#    val1 = int(input("Enter a number: "))
+#    print(val1)
+#except Exception as error:
+#    print("ERROR MESSAGE: ", error)
+
+#items  = [1, 2, 3, 4, 5, 'word', 6, 7, 8, 9]
+#for item in items:
+#    try:
+#        print(int(item))
+#   except Exception as e:
+#        pass #to skip the only str 'word'
+
+#class Operator:
+#    def add():
+#        print("Add")
+
+#    def sub():
+#        print("Sub")
+    
+#def parse(input):
+#    return input.split(',')
+
+#if __name__ == '__main__':
+#    op = Operator
+#    op.add()
+
+#    str_comb = "1m, 12, 1, 1, 1, 1, 12, 3, 4, 1234"
+#    parsed_str = parse(str_comb)
+#    print(parsed_str)
+
+
+#ABSOLURE VALUE
+#val1 = -1
+#print(abs(val1))
+
+
+#list = []
+#values = int(input("Enter values you want to multiply: "))
+
+#prod = values.split()
+
+
+#for i in range(len(prod)):
+
+
+#to be continued...
+#import math
+#values = input('Enter values you want to multiply: ')
+
+#result = values.split()
+#for i in range(len(result)):
+#    result[i] = int(result[i])
+        
+#    result = math.prod(int(values))
+
+#print(result)
+
