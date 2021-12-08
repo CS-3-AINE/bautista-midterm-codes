21b3b5a247812374a1c8f8f3c75eb29a10ba91e0
A. Bautista <bautista_3191017@letran-calamba.edu.ph>
2021-12-08 09:51:11 +0800

    nov_09_codes2

---
+++ bautista-midterm-codes/nov_09_notes2.py
@@ -0,0 +1,30 @@
+#FUNCTIONS
+#def add (val1, val2):
+   # return val1 + val2
+
+#def sub(val1, val2):
+  #  return val1 - val2
+
+#sum = add(10, 14)
+#print("The sum is " + str(sum))
+#print(f"The sum is {sum}")
+
+#diff = sub(10, 20)
+#print("The difference is" + str(diff))
+
+#def add(*args): #argument
+   # return args[0] + args[1] + args[2]
+
+#if __name__== "__main__":
+ #   sum = add(12, 10, 2)
+
+    #print(f"The sum is {sum}") 
+ 
+
+#def sub(**kwargs): #key arguments
+#    val1 = kwargs['val1']
+#    val2 = kwargs['val2']
+#    val3 = kwargs['val3']
+#if __name__== "__main__":
+#    diff = sub(val1 = 1, val2 = 3, val3 = 10)
+#    print(f"The difference is {diff}")
